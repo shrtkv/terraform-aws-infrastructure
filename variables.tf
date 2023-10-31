7c98eb0d416af21735ec058a7ffef7a3144b6466
tokoshiro5 <tokoshiro5@gmail.com>
2023-10-31 12:06:19 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,6 +27,7 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
 variable "key_name" {
   description = "The name of the SSH key pair to associate with the EC2 instance."
   type        = string
